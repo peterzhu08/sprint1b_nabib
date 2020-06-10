50b78d2c7a4f8ad8b3f8d9089fe4cc01f3b2b095
Peter Zhu <peterzhu@berkeley.edu>
2020-06-10 16:36:03 -0700

    Created from Oracle OCE Server - first commit

---
+++ command.sh
@@ -0,0 +1,18 @@
+# NOTE: username:password will expire Nov 20th. Use ""
+curl -u 'peterzhu08':'SciOly4evr' https://api.github.com/user/repos -d '{"name":"sprint1d_nabib"}'
+git init
+git clone https://github.com/nahmed3536/sprint1.git
+pwd
+git add .
+pwd 
+git status
+pwd
+git commit -m "Created from Oracle OCE Server - first commit"
+pwd
+git remote add origin https://github.com/peterzhu08/sprint1d_nabib.git
+pwd
+git remote -v
+pwd
+git push -u origin master
+pwd
+cmd /k
